fd0ca706f9cf940feb9560a06879d88dd4926f61
haneuljj <wjsckstj33@gmail.com>
2024-06-20 16:24:53 +0900

    change some paths

--- src/App.js
+++ src/App.js
@@ -9,8 +9,8 @@ import Home from "./routes/Home";
 function App() {
   return <Router>
     <Routes>
-      <Route path="/movie/:id" element={<Detail />}></Route>
-      <Route path="/" element={<Home />}></Route>
+      <Route path={`${process.env.PUBLIC_URL}/movie/:id`} element={<Detail />}></Route>
+      <Route path={`${process.env.PUBLIC_URL}/`} element={<Home />}></Route>
     </Routes>
   </Router>;
 }
